3f3456f34f356de8aea2ad3b47b9574e5f96ae66
Mark Cohen <git@mail.mpc.sh>
2020-01-03 16:52:50 -0600

    Import styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,14 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('fonts');
   eleventyConfig.addPassthroughCopy('img');
+
+  eleventyConfig.addFilter('dateFormat', date => {
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      timeZone: 'UTC',
+    }).format(date);
+  });
 };
