e292aab12bf8eded3faaf12dd58b29bbb806503f
Mark Cohen <git@mail.mpc.sh>
2022-03-07 17:04:24 -0800

    Run prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,29 @@
-const description = require('eleventy-plugin-description');
-const readingTime = require('reading-time');
+const description = require("eleventy-plugin-description");
+const readingTime = require("reading-time");
 
-module.exports = function(config) {
-  config.addPassthroughCopy('src/fonts');
-  config.addPassthroughCopy('src/img');
+module.exports = function (config) {
+	config.addPassthroughCopy("src/fonts");
+	config.addPassthroughCopy("src/img");
 
-  config.addFilter('dateFormat', date =>
-    new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      timeZone: 'UTC',
-    }).format(date),
-  );
+	config.addFilter("dateFormat", (date) =>
+		new Intl.DateTimeFormat("en-US", {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			timeZone: "UTC",
+		}).format(date),
+	);
 
-  config.addPlugin(description);
+	config.addPlugin(description);
 
-  config.addFilter('readingTime', templateContent =>
-    Math.ceil(readingTime(templateContent).minutes),
-  );
+	config.addFilter("readingTime", (templateContent) =>
+		Math.ceil(readingTime(templateContent).minutes),
+	);
 
-  return {
-    dir: {
-      input: "src",
-      output: "build"
-    }
-  };
+	return {
+		dir: {
+			input: "src",
+			output: "build",
+		},
+	};
 };
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 100,
+	useTabs: true,
+	semi: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	bracketSameLine: true,
+	arrowParens: "always",
+	proseWrap: "always",
+};
